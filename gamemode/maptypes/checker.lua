cc7ae20ba84d5e4b4698e8eb182d0dd06c41dc78
benjaminpants <maltbyfamily@gmail.com>
2022-05-30 03:06:43 -0700

    checkerboard

---
+++ gamemode/maptypes/checker.lua
@@ -0,0 +1,27 @@
+map.name = "Checkerboard";
+map.description = "Wanna play some checkers?"
+
+function map:generateMap(grid)
+	local i = 1
+	for x = grid.minx, grid.maxx do
+		for y = grid.miny, grid.maxy do
+			i = i + 1
+			if i % 2 == 0 then
+				grid:setHardBox(x, y)
+			else
+				if (math.random(1,5) ~= 1) then
+					grid:setBox(x, y)
+				else
+					grid:setExplosiveBox(x, y)
+				end
+			end
+		end 
+	end
+
+	for i=1, math.Clamp(math.ceil(((grid.maxx + grid.maxy) / 4)),1,999) do
+		local x = math.random(grid.minx, grid.maxx)
+		local y = math.random(grid.miny, grid.maxy)
+		grid:setEmpty(x, y)
+	end
+
+end
